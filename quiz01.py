7dccb7580b8f9db351f48aff43b98faa790a179e
kimikazu Kato <kimikazu@gmail.com>
2017-06-09 11:30:55 +0900

    Added quiz

---
+++ quiz01.py
@@ -0,0 +1,34 @@
+def f(a, b):
+    # 数値aとbの和を計算しxに代入せよ
+
+    # （次の行を書き換える）
+    x = 0
+    return x
+
+
+def g(a, b):
+    # 数値aとbの積を計算し、xに代入せよ
+
+    # （次の行を書き換える）
+    x = 0
+    return x
+
+assert f(3, 5) == def f(a, b):
+    # 数値aとbの和を計算しxに代入せよ
+
+    # （次の行を書き換える）
+    x = 0
+    return x
+
+
+def g(a, b):
+    # 数値aとbの積を計算し、xに代入せよ
+
+    # （次の行を書き換える）
+    x = 0
+    return x
+
+assert f(3, 5) == 8
+assert g(2, 3) == 6
+8
+assert g(2, 3) == 6
